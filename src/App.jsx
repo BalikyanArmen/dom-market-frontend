fd51b9363922ca6f76df9a654395ae2249f10469
BalikyanArmen <150682351+BalikyanArmen@users.noreply.github.com>
2024-01-22 17:30:32 +0400

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,19 @@
-// App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import Box from '@mui/material/Box';
-// import Container from '@mui/material/Container';
-import AppBar from '@mui/material/AppBar';
-import Stack from '@mui/material/Stack';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import "./styles/mainStyles.scss";
+import { Button } from "@mui/material";
+import LocationIcon from "./assets/locationIcon.svg";
+import {HomepageCategory} from "./components/HomepageCategory.jsx";
+import {ProductPage} from "./components/ProductPage.jsx";
+import {CategoryPageLeftCategories} from "./components/CategoryPage.jsx"; // Import the SVG file
 
-const darkTheme = createTheme({
-    palette: {
-        mode: 'dark',
-        primary: {
-            main: '#1976d2',
-        },
-    },
-});
 const App = () => {
     return (
         <Router>
             <div>
-                <Stack spacing={2} sx={{ flexGrow: 1 }}>
-                    <ThemeProvider theme={darkTheme}>
-                        <AppBar position="static" color="primary" enableColorOnDark>
-                            <Toolbar>
-                                <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-                                    <MenuIcon />
-                                </IconButton>
-                                <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-                                    {'enableColorOnDark'}
-                                </Typography>
-                            </Toolbar>
-                        </AppBar>
-
-                    </ThemeProvider>
-                </Stack>
-
+                <CategoryPageLeftCategories/>
                 <nav>
                     <ul>
                         <li>
